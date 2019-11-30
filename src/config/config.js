2a1ada36810119f38d477ab946ac3c4e6e91d648
muhammadzubairmoosani <muhammadzubairmoosani@gmail.com>
2019-11-30 15:14:09 +0500

    first commit

--- src/config/config.js
+++ src/config/config.js
@@ -1,7 +1,5 @@
 import * as firebase from 'firebase';
-// import admin from 'firebase-admin';
-
-var firebaseConfig = {
+const firebaseConfig = {
     apiKey: "AIzaSyDNW1xIoDbjhKuftH4ORCF_mH1Ezt23T5g",
     authDomain: "campus-recruitment-syste-846b4.firebaseapp.com",
     databaseURL: "https://campus-recruitment-syste-846b4.firebaseio.com",
@@ -11,8 +9,5 @@ var firebaseConfig = {
     appId: "1:1076130726822:web:4c749a6a97bd77a4724567",
     measurementId: "G-XSC31PYNKR"
 };
-
 firebase.initializeApp(firebaseConfig);
-
 export default firebase;
-// export { firebase, admin };
